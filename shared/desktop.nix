bae1f45844b9993cc22f6956695ce3428fd32ba5
Paulo <git@pauloo27.dev>
2025-12-31 21:42:22 -0300

    feat: default browser

--- shared/desktop.nix
+++ shared/desktop.nix
@@ -2,4 +2,15 @@
   home.packages = [
     pkgs.wl-clipboard
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+    };
+  };
 }
