7a86d028176dd582f7d0de584746733d05dbeea5
Paulo <git@pauloo27.dev>
2026-01-06 00:21:44 -0300

    feat: karen git config?

--- hosts/karen/home.nix
+++ hosts/karen/home.nix
@@ -2,10 +2,32 @@
   imports = [
     ../../shared/base.nix
     ../../shared/zsh.nix
-    ../../shared/git.nix
     ../../shared/tmux.nix
     ../../shared/linux.nix
   ];
+
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+        name = "karen admin";
+        email = "admin@karen.db.cafe";
+      };
+      core = {
+        editor = "nvim --cmd 'let g:unception_block_while_host_edits=1'";
+      };
+      pull = {
+        rebase = false;
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      rerere = {
+        enabled = true;
+      };
+    };
+  };
 
   home.packages = [ ];
 
