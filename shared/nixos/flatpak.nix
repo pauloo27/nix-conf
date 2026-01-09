04dfecbd2f2b7df670a9068851f2aec9ef9ff3d8
Paulo <git@pauloo27.dev>
2026-01-09 17:56:08 -0300

    ref: move mime options closer to flatpak

--- shared/nixos/flatpak.nix
+++ shared/nixos/flatpak.nix
@@ -10,4 +10,15 @@
       onCalendar = "daily";
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/http" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/https" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/about" = "app.zen_browser.zen.desktop";
+      "x-scheme-handler/unknown" = "app.zen_browser.zen.desktop";
+    };
+  };
 }
