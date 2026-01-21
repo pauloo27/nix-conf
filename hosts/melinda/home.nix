e40431c8d42a2083478b8b582fd5491f500b8727
Paulo <git@pauloo27.dev>
2026-01-21 18:45:33 -0300

    feat: melinda overrides and tidy

--- hosts/melinda/home.nix
+++ hosts/melinda/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ../../shared/base.nix
@@ -16,7 +16,25 @@
   ];
 
   # Override caps:swapescape for desktop (not needed with custom keyboard)
-  wayland.windowManager.hyprland.settings.input.kb_options = "";
+  wayland.windowManager.hyprland.settings.input.kb_options = lib.mkForce "";
+
+  # Monitor setup
+  wayland.windowManager.hyprland.settings.monitor = [
+    "DP-1,1920x1080@144,1080x600,1"
+    "HDMI-A-1,2560x1080@75,0x0,1,transform,1"
+  ];
+
+  # Override wallpaper
+  services.hyprpaper.settings = lib.mkForce {
+    preload = [
+      "/home/paulo/medias/pictures/wallpapers/enabled/1.jpg"
+      "/home/paulo/medias/pictures/wallpapers/enabled/2.png"
+    ];
+    wallpaper = [
+      "HDMI-A-1,/home/paulo/medias/pictures/wallpapers/enabled/1.jpg"
+      "DP-1,/home/paulo/medias/pictures/wallpapers/enabled/2.png"
+    ];
+  };
 
   home.stateVersion = "25.05";
   home.username = "paulo";
