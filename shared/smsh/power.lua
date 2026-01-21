913277131679f9da57d147816227c32de56ca6b9
Paulo <git@pauloo27.dev>
2026-01-20 21:35:28 -0300

    feat: only show "reboot into windows" for the windows machine

--- shared/smsh/power.lua
+++ shared/smsh/power.lua
@@ -3,6 +3,88 @@ require("wrapper")
 
 load_stylesheet("./style.css")
 
+local hostname = io.popen("hostname"):read("*l")
+local has_windows = hostname == "melinda"
+
+local options = {
+	{
+		type = "button",
+		text = "",
+		tooltip = "Lock",
+		action = function()
+			os.execute("hyprlock &")
+			os.exit(0)
+		end,
+	},
+	{
+		type = "button",
+		text = "",
+		tooltip = "Shutdown",
+		action = function()
+			confirm({
+				message = "Shutdown?",
+				present = true,
+				exit_on_close = true,
+				on_yes = function()
+					run_unless_pacman("shutdown now")
+					os.exit(0)
+				end,
+			})
+		end,
+	},
+	{
+		type = "button",
+		text = "",
+		tooltip = "Reboot",
+		action = function()
+			confirm({
+				message = "Reboot?",
+				present = true,
+				exit_on_close = true,
+				on_yes = function()
+					run_unless_pacman("reboot")
+					os.exit(0)
+				end,
+			})
+		end,
+	},
+	{
+		type = "button",
+		text = "󰗽",
+		tooltip = "Logout",
+		action = function()
+			confirm({
+				message = "Logout?",
+				present = true,
+				exit_on_close = true,
+				on_yes = function()
+					run_unless_pacman("hyprctl dispatch exit exit || swaymsg exit")
+					os.exit(0)
+				end,
+			})
+		end,
+	},
+}
+
+if has_windows then
+	table.insert(options, {
+		type = "button",
+		text = "",
+		tooltip = "Reboot into Windows",
+		action = function()
+			confirm({
+				message = "Reboot into Windows?",
+				present = true,
+				exit_on_close = true,
+				on_yes = function()
+					run_unless_pacman("~/.config/scripts/reboot-into-windows.sh")
+					os.exit(0)
+				end,
+			})
+		end,
+	})
+end
+
 new_window({
 	title = "SMSH: Power",
 	enable_vim_keys = true,
@@ -12,80 +94,6 @@ new_window({
 	root = {
 		type = "container",
 		orientation = "vertical",
-		children = {
-			{
-				type = "button",
-				text = "",
-				tooltip = "Lock",
-				action = function()
-					os.execute("hyprlock &")
-					os.exit(0)
-				end,
-			},
-			{
-				type = "button",
-				text = "",
-				tooltip = "Shutdown",
-				action = function()
-					confirm({
-						message = "Shutdown?",
-						present = true,
-						exit_on_close = true,
-						on_yes = function()
-							run_unless_pacman("shutdown now")
-							os.exit(0)
-						end,
-					})
-				end,
-			},
-			{
-				type = "button",
-				text = "",
-				tooltip = "Reboot",
-				action = function()
-					confirm({
-						message = "Reboot?",
-						present = true,
-						exit_on_close = true,
-						on_yes = function()
-							run_unless_pacman("reboot")
-							os.exit(0)
-						end,
-					})
-				end,
-			},
-			{
-				type = "button",
-				text = "",
-				tooltip = "Reboot into Windows",
-				action = function()
-					confirm({
-						message = "Reboot into Windows?",
-						present = true,
-						exit_on_close = true,
-						on_yes = function()
-							run_unless_pacman("~/.config/scripts/reboot-into-windows.sh")
-							os.exit(0)
-						end,
-					})
-				end,
-			},
-			{
-				type = "button",
-				text = "󰗽",
-				tooltip = "Logout",
-				action = function()
-					confirm({
-						message = "Logout?",
-						present = true,
-						exit_on_close = true,
-						on_yes = function()
-							run_unless_pacman("hyprctl dispatch exit exit || swaymsg exit")
-							os.exit(0)
-						end,
-					})
-				end,
-			},
-		},
+		children = options,
 	},
 })
