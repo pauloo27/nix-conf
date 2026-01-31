aa6091fa31131c8583683411b306bec175476bfa
Paulo <git@pauloo27.dev>
2026-01-31 13:48:50 -0300

    fix(tmux): vim mode and etc

--- shared/home/tmux.nix
+++ shared/home/tmux.nix
@@ -33,8 +33,9 @@
       set -g status-right ' #(whoami)@#H '
 
       # vim-like visual mode
+      set-window-option -g mode-keys vi
       bind -T copy-mode-vi v send-keys -X begin-selection
-      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'wl-copy'
 
       # nvim <-> tmux
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?\.?(view|n?vim?x?)(-wrapped)?(diff)?$'"
