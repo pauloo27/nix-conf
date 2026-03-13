3ac1ba577675d920e5f0225f2a2dd56c268e29af
Paulo <git@pauloo27.dev>
2026-03-12 22:55:20 -0300

    stuff + update

--- shared/home/editor/nvim/plugins/blink.lua
+++ shared/home/editor/nvim/plugins/blink.lua
@@ -9,4 +9,14 @@ require('blink.cmp').setup({
   sources = {
     default = { 'lsp' },
   },
+  completion = {
+    menu = {
+      auto_show = true,
+      draw = {
+        columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'source_name' } },
+      }
+    },
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    ghost_text = { enabled = true },
+  },
 })
