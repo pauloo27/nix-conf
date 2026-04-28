36efc50c0d69e45e37d99f9f144ffbe6cce65f3e
Paulo <git@pauloo27.dev>
2026-04-28 13:40:22 -0300

    fix: unbreak

--- shared/home/apps/voxtype-cpu.nix
+++ shared/home/apps/voxtype-cpu.nix
@@ -3,6 +3,5 @@
   home.packages = [ unstable.voxtype-onnx ];
   home.sessionVariables = {
     ALSA_PLUGIN_DIR = "/usr/lib64/alsa-lib:/usr/lib/alsa-lib";
-    LD_LIBRARY_PATH = "/usr/lib64:/usr/lib\${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}";
   };
 }
