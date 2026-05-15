96ab148d7c49d209268aa67551edebdd0fe9424e
Paulo <git@pauloo27.dev>
2026-05-15 11:38:00 -0300

    bun

---
+++ dev/bun/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Bun development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+            ];
+          };
+        }
+      );
+    };
+}
