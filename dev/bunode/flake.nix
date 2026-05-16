2de715d0aaa24a4c5b10f4a255e91687a534292c
Paulo <git@pauloo27.dev>
2026-05-15 22:01:12 -0300

    feat: bun AND node

---
+++ dev/bunode/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Bun + Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              nodejs_24
+              eslint_d
+              biome
+              nodePackages.npm
+              nodePackages.pnpm
+              nodePackages.yarn
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+            ];
+          };
+        }
+      );
+    };
+}
