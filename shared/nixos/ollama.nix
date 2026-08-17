3db59eb6221f2cd2c705283ee9b8dbb73a7af31d
kraken admin <admin@kraken.db.cafe>
2026-08-16 23:52:42 -0300

    ollama on the lan

--- shared/nixos/ollama.nix
+++ shared/nixos/ollama.nix
@@ -2,5 +2,11 @@
 {
   services.ollama = {
     enable = true;
+    # kraken.ndo.cafe -> tailscale0
+    host = "100.123.26.28";
   };
+
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i tailscale0 -p tcp --dport 11434 -j ACCEPT
+  '';
 }
